1e91a1bd42e7037c13b075a1bbf06f684e37238f
euglazer <eugenestapler@gmail.com>
2016-05-19 16:29:19 -0700

    add help file

--- bin/help.js
+++ bin/help.js
@@ -1,3 +1,11 @@
 module.exports = function () {
-  console.log('heard u need help lol')
+  console.log(
+    'usage: memories <command> \n' +
+    '\n' +
+    'commands: \n' +
+    '  init    sets preferred editor and path for journal entries \n' +
+    '  add     opens today\'s journal file \n' +
+    '  help    displays this help file' +
+    '\n'
+  )
 }
