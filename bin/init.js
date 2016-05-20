acfc9b612901255b4ce5c6534afafc9f6da7882e
euglazer <eugenestapler@gmail.com>
2016-05-19 18:16:20 -0700

    during init, display selected editor if it exists

--- bin/init.js
+++ bin/init.js
@@ -5,6 +5,9 @@ var homedir = require('homedir')
 var isThere = require('is-there')
 
 module.exports = function () {
+  var configPath = homedir() + '/.memories.js'
+  var config = isThere(configPath) ? require(configPath) : {}
+
   var questions = [
     {
       type: 'input',
@@ -15,8 +18,8 @@ module.exports = function () {
     {
       type: 'input',
       name: 'editor',
-      message: 'and what editor would you like to use to log your memories',
-      default: 'atom'
+      message: 'and what editor would you like to use to log your memories?',
+      default: config.editor || 'atom'
     }
   ]
 
